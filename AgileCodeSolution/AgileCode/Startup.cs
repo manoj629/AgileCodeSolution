4af2d67b2395bb10853e048cfedd2fde28e8a008
manoj629 <manojpaliwal06@gmail.com>
2017-03-20 14:44:16 +0530

    first commit

---
+++ AgileCodeSolution/AgileCode/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AgileCode.Startup))]
+namespace AgileCode
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
